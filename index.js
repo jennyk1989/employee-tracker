450f8969e702aefb0d7014dfd693156f8e8b582f
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-24 19:45:15 -0600

    pseudocode added; js file name changed

---
+++ index.js
@@ -0,0 +1,15 @@
+//start app & given options to view all departments, roles, or employees; add a department, role, or employee; update an employee role  
+
+// choose view departments -> given formatted table showing deparments names & ids
+
+// choose view roles -> given job title, role id, department of role, salary for role
+
+// choose view employees -> given table showing employee data (ids, first & last names, job title, deparments, salaries, and their managers)
+
+// choose add department --> prompted to enter name of department & it's added to database
+
+// choose add role -> prompted to enter name, salary, department of role and role is added to db
+
+// choose add employee -> prompted to enter their 1st & last name, role, manager & then employee is added to db
+
+// choose update employee role -> prompted to select employee to update & their new role & this info is updated in the db
