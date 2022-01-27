6469db42c601c67f2abba0eee2d29d67af14f9cc
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-26 19:36:24 -0600

    added seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL 
+    department_name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE role (
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (id, department_name)
+VALUES  
+(1, "Defense Against the Dark Arts"),
+(2, "Charms"),
+(3, "Muggle Studies"),
+(4, "Potions"),
+(5, "Transfiguration");
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES
+(1155, "Hermione", "Granger", 11100),
+(2244, "Harry", "Potter", 22299),
+(3333, "Luna", "Lovegood", 33388),
+(4422, "Lucius", "Malfoy", 44477),
+(5511, "Ron", "Weasley", 55566);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+(11, "Minister of Magic", 550000, 111),
+(22, "Auror", 380000, 222),
+(33, "Healer", 250000, 333),
+(44, "Unspeakable", 200000, 444),
+(55, "Junior Minister", 180000, 555);
+
