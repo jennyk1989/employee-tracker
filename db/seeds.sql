dd69c5c35298d60f5fcf137dffbf97aaf96c09fa
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-27 23:59:36 -0600

    seeds update

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,19 +6,20 @@ VALUES
 (4, "Potions"),
 (5, "Transfiguration");
 
-INSERT INTO employee (id, first_name, last_name, role_id)
-VALUES
-(1155, "Hermione", "Granger", 11100),
-(2244, "Harry", "Potter", 22299),
-(3333, "Luna", "Lovegood", 33388),
-(4422, "Lucius", "Malfoy", 44477),
-(5511, "Ron", "Weasley", 55566);
-
 INSERT INTO role (id, title, salary, department_id)
 VALUES
-(11, "Minister of Magic", 550000, 111),
-(22, "Auror", 380000, 222),
-(33, "Healer", 250000, 333),
-(44, "Unspeakable", 200000, 444),
-(55, "Junior Minister", 180000, 555);
+(1, "Minister of Magic", 550000, 111),
+(2, "Auror", 380000, 222),
+(3, "Healer", 250000, 333),
+(4, "Unspeakable", 200000, 444),
+(5, "Junior Minister", 180000, 555);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES
+(1, "Hermione", "Granger", 11100),
+(2, "Harry", "Potter", 22299),
+(3, "Luna", "Lovegood", 33388),
+(4, "Lucius", "Malfoy", 44477),
+(5, "Ron", "Weasley", 55566);
+
 
